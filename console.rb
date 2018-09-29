86f7f3f0bfd64489c1d2b647efd27d67a26e69c9
ionawright <ionawright@live.com>
2018-09-29 16:05:36 +0100

    console_oject_update

--- console.rb
+++ console.rb
@@ -3,7 +3,36 @@ require_relative("./models/film.rb")
 require_relative("./models/ticket.rb")
 require('pry')
 
+#--
+  customer1 = Customer.new({
+    "name" => "Jordan Pollock",
+    "funds" => 12
+    })
 
+  customer1 = Customer.new({
+    "name" => "Calum MacKinnon",
+    "funds" => 10
+    })
+#--
+  film1 = Film.new ({
+    "title" => "King of Thieves",
+    "price" => 10
+    })
 
+  film2 = Film.new ({
+    "title" => "A Simple Favor",
+    "price" => 8
+    })
+#--
+  ticket1 = Ticket.new ({
+    "customer_id" => customer1.id,
+    "film_id" => film1.id
+    })
+
+  ticket2 = Ticket.new ({
+    "customer_id" => customer2.id,
+    "film_id" => film2.id
+    })
+#--
 binding.pry
 Going to the movies
